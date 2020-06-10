9225ade6822035716e6e2da65ab5d8c748187dac
Samuel Njenga <snjenga704@gmail.com>
2020-06-10 20:17:39 +0300

    Added 4-fat-functions/1-noob.js and api/users.js
    Single function doing validation,error printing and saving

---
+++ 4-fat-functions/1-noob.js
@@ -0,0 +1,42 @@
+const {
+    updateUser,
+    createUser
+} = require('./api/users')
+
+function saveUser(user) {
+    const errors = []
+    if (user.username) {
+        if (user.username.length < 3) {
+            errors.push("Username must be 3 or more characters")
+        }
+    } else {
+        errors.push("Username is required")
+    }
+
+    if (user.password) {
+        if (user.password.length < 8) {
+            errors.push("Password must be 8 or more characters")
+        }
+    } else {
+        errors.push("Password is required")
+    }
+
+    if (errors.length > 0) {
+        errors.forEach(error => console.error(error))
+        return
+    }
+    if(user.id == null){
+        console.log('Created User')
+        createUser(user)
+    } else{
+        console.log('Updated User')
+        updatedUser(user)
+    }
+}
+
+const user = {
+    username: 'WDS',
+    password: 'password'
+}
+
+saveUser(user)
