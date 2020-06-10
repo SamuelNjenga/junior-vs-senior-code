1e7a014aa5fd201a38a2a0e74d179ff1d8ad6207
Samuel Njenga <snjenga704@gmail.com>
2020-06-10 22:12:10 +0300

    Included pro/validation.js and pro.js
    Adopt Single Responsibility in my functions as well as Breaking up larger functions into more smaller ones

---
+++ 4-fat-functions/3-pro.js
@@ -0,0 +1,43 @@
+const {updateUser,createUser} = require('./api/users')
+const {validationMessages,printErrors} = require('./pro/validation')
+
+function saveUser(user) {
+    if (user.id == null) {
+        console.log('Created User')
+        createUser(user)
+    } else {
+        console.log('Updated User')
+        updatedUser(user)
+    }
+}
+
+function validateUser(user){
+    const validations = {
+        username:{
+            required:true,
+            length:3
+        },
+        password:{
+            required:true,
+            length:8
+        }
+    }
+    const errors = validationMessages(validations,user);
+    return {
+        valid:Object.values(errors).every(messages => messages.length === 0),
+        errors:errors
+    }
+}
+
+const user = {
+    id:1,
+    username: '',
+    password: 'password'
+}
+
+const {errors,valid} = validateUser(user)
+if(valid){
+    saveUser(user)
+} else{
+    printErrors(errors)
+}
